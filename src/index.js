7ba3eb04a4d8212710cf51c6cb1648a9f44fe775
Jorge García González <jorge@MacBook-Pro-de-Jorge-2.local>
2017-04-20 17:37:11 +0200

    console

--- src/index.js
+++ src/index.js
@@ -86,6 +86,7 @@ const initVueRouterGuard = function (Vue, vueRouter, ignoredViews) {
 
     // Dispatch vue event using meta analytics value if defined otherwise fallback to route name
     //Vue.analytics.trackView(to.meta.analytics || to.name)
+    console.log(to)
     Vue.analytics.pageView(to.meta.analytics || to.name)
   })
 
